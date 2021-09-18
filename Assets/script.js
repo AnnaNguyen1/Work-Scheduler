24b05b640520986e75fc7785b4f35abb74c12f49
Anna <an9062@gmail.com>
2021-09-18 13:46:08 +1000

    added comment to the for loop

--- Assets/script.js
+++ Assets/script.js
@@ -5,11 +5,13 @@ console.log(typeof $(".time-block"));
 var timeBlock = $(".time-block"); // object
 var timeDif = 9; 
 
+
+// Create a for loop to check if the hour (-9) matches with the timeBlock index
 function colourBlock(){
     var hour = moment().hours(); // number (24hrs)
 
     for (i = 0; i < timeBlock.length; i++) {
-        if (i = i) {
+        if (i = (hour - timeDif)) {
             console.log("hi");
         }
     }
