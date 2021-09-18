f8e89d6b4de980117acf2b57fb5f8ed765bfd8ea
Anna <an9062@gmail.com>
2021-09-18 16:18:26 +1000

    changed id down to textarea in HTML and added this statement into each addClass function

--- Assets/script.js
+++ Assets/script.js
@@ -1,24 +1,26 @@
 $("#currentDay").text(moment().format("dddd Do MMM, YYYY"));
 
-console.log(typeof $(".text-area"));
+var textArea = $(".text-area"); 
+var currentHour = moment().hour(); // Number in 24hr
 
-var timeBlock = $(".time-block");
-var textArea = $(".text-area"); // object
-var currentHour = moment().hour(); //number in 24hr
+// Create an each() loop to check if the hour matches with the timeBlock element
+function colorSelection() {
+    textArea.each(function() {
+        // $(this) is required to select the index of the element from timeBlock
+        // Parse String to Integer
+        var thisTextArea = parseInt($(this).attr("id")); 
 
-// Create a jquery .each() loop to check if the hour matches with the timeBlock element
-function colorBlock() {
-    timeBlock.each(function(){
-        if (parseInt(timeBlock.attr("id")) === currentHour) { // Parse string to integer
-            textArea.addClass("present");
-        } else if (parseInt(timeBlock.attr("id")) < currentHour) {
-            textArea.addClass("past");
-            console.log(parseInt(timeBlock.attr("id")));
-            console.log(currentHour);
-        } else {
-            textArea.addClass("future");
+        if (thisTextArea < currentHour) { 
+            $(this).addClass("past");
+        } 
+        else if (thisTextArea === currentHour) {
+            $(this).addClass("present");
+        } 
+        else {
+            $(this).addClass("future");
+            console.log(currentHour, thisTextArea);
         }
     })
 };
 
-colorBlock();
+colorSelection();
