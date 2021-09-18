ba342351d4ca28b506bfd4b16a15dda8c3ca8d3e
Anna <an9062@gmail.com>
2021-09-18 15:15:24 +1000

    Added .each() loop to go through every element to find an id match

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,24 @@
 $("#currentDay").text(moment().format("dddd Do MMM, YYYY"));
 
-console.log(typeof $(".time-block"));
+console.log(typeof $(".text-area"));
 
-var timeBlock = $(".time-block"); // object
-var timeDif = 9; 
+var timeBlock = $(".time-block");
+var textArea = $(".text-area"); // object
+var currentHour = moment().hour(); //number in 24hr
 
-
-// Create a for loop to check if the hour (-9) matches with the timeBlock index
-function colourBlock(){
-    var hour = moment().hours(); // number (24hrs)
-
-    for (i = 0; i < timeBlock.length; i++) {
-        if (i = (hour - timeDif)) {
-            console.log("hi");
+// Create a jquery .each() loop to check if the hour matches with the timeBlock element
+function colorBlock() {
+    timeBlock.each(function(){
+        if (parseInt(timeBlock.attr("id")) === currentHour) { // Parse string to integer
+            textArea.addClass("present");
+        } else if (parseInt(timeBlock.attr("id")) < currentHour) {
+            textArea.addClass("past");
+            console.log(parseInt(timeBlock.attr("id")));
+            console.log(currentHour);
+        } else {
+            textArea.addClass("future");
         }
-    }
+    })
+};
 
-}
+colorBlock();
