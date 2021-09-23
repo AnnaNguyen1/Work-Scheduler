2d9c2db651315c69cc533efd194c2d3068ee5ca3
Anna <an9062@gmail.com>
2021-09-23 10:19:09 +1000

    added README file and cleaned up code

--- Assets/script.js
+++ Assets/script.js
@@ -1,26 +1,55 @@
-$("#currentDay").text(moment().format("dddd Do MMM, YYYY"));
-
 var textArea = $(".text-area"); 
 var currentHour = moment().hour(); // Number in 24hr
+var workTime = $(".hour"); 
 
-// Create an each() loop to check if the hour matches with the timeBlock element
+// Display current date in header
+$("#currentDay").text(moment().format("dddd Do MMM, YYYY"));
+
+// Functions
+colorSelection();
+loadPage();
+
+// Create an each() loop to check if the hour matches with the textArea element
 function colorSelection() {
     textArea.each(function() {
-        // $(this) is required to select the index of the element from timeBlock
-        // Parse String to Integer
+        // $(this) is required to select the index of the element from textArea
+        // Parse String to Integer or else it won't match with current hour
         var thisTextArea = parseInt($(this).attr("id")); 
 
         if (thisTextArea < currentHour) { 
             $(this).addClass("past");
-        } 
-        else if (thisTextArea === currentHour) {
+        } else if (thisTextArea === currentHour) {
             $(this).addClass("present");
-        } 
-        else {
+        } else {
             $(this).addClass("future");
-            console.log(currentHour, thisTextArea);
+            // console.log(currentHour, thisTextArea);
         }
     })
 };
 
-colorSelection();
+// Save time and text in local storage 
+$(".saveBtn").on("click", function() {
+
+    // console.log(this);
+    // Use "this" to call each of the same element and siblings to call other elements
+    var time = $(this).siblings(".hour").text();
+    var text = $(this).siblings(".text-area").val().trim();
+
+    localStorage.setItem(time, text);  // 9AM | `text`
+})
+
+// Load text onto screen from local storage 
+function loadPage() {
+    // Use each() to get key from local storage based on "hour"
+    workTime.each(function() {
+        // Index for each hour (Index will be text in this case)
+        var workHour = $(this).text();
+        // console.log(workHour);
+        // console.log(localStorage.getItem(workHour)) --> empty string returns null
+
+        if (workHour != null) {
+            $(this).siblings(textArea).val(localStorage.getItem(workHour));
+        } else
+        return;
+    })
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+## Work Day Planner
+
+Work Day Planner is used to enter in tasks or memos for the dat. 
+It will display the current date at the top of the screen, and the colours of the text field will be updated according to the time of the day. 
+The memo will be saved in local storage if the user clicks on the Save button. 
+
+# Preview of the planner
+
+![Users can enter in the plan for the day and save it in local storage.](./Assets/images/preview.bmp)
+
+
+# Deployed application
+
+https://annanguyen1.github.io/Work-Scheduler/
+
